4539cfa1c77fcc57dfeb30a2fadd072efb5ff8af
Anna Sytenko <anna_sytenko91@icloud.com>
2026-03-27 20:50:24 +0700

    added reviewers to list component and stylized

--- components/Psychologists/PsychologistCard.tsx
+++ components/Psychologists/PsychologistCard.tsx
@@ -1,11 +1,13 @@
 import { Psychologist } from "@/types/psychologists";
 import Image from "next/image";
 import css from "./PsychologistsCard.module.css";
+import { useState } from "react";
 
 export default function PsychologistCard({ item }: { item: Psychologist }) {
+  const [expanded, setExpanded] = useState(false);
   return (
     // <div className="container">
-    <div className={css.card}>
+    <div className={`${css.card} ${expanded ? css.expanded : ""}`}>
       <div className={css.rightSide}>
         <Image
           src={item.avatar_url}
@@ -16,28 +18,30 @@ export default function PsychologistCard({ item }: { item: Psychologist }) {
         />
       </div>
       <div className={css.leftSide}>
-        <div className={css.heart}>
-          <svg
-            width="25"
-            height="22"
-            viewBox="0 0 25 22"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M21.8989 2.74614C21.3456 2.19257 20.6886 1.75343 19.9655 1.45382C19.2425 1.15421 18.4674 1 17.6847 1C16.902 1 16.127 1.15421 15.4039 1.45382C14.6809 1.75343 14.0239 2.19257 13.4706 2.74614L12.3222 3.89448L11.1739 2.74614C10.0562 1.62848 8.54035 1.00058 6.95973 1.00058C5.37911 1.00058 3.86323 1.62848 2.74557 2.74614C1.6279 3.86381 1 5.37969 1 6.96031C1 8.54093 1.6279 10.0568 2.74557 11.1745L3.8939 12.3228L12.3222 20.7511L20.7506 12.3228L21.8989 11.1745C22.4525 10.6212 22.8916 9.96419 23.1912 9.24111C23.4908 8.51803 23.645 7.74301 23.645 6.96031C23.645 6.17762 23.4908 5.40259 23.1912 4.67951C22.8916 3.95643 22.4525 3.29946 21.8989 2.74614Z"
-              stroke="#191A15"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
-        </div>
         <div className={css.top}>
           <p className={css.text}>Psychologist</p>
-          <div className={css.ratingContainer}>
-            <p className={css.nextRating}>⭐ {item.rating}</p>
-            <p className={css.textPrice}>${item.price_per_hour}/hour</p>
+          <div className={css.rightTop}>
+            <div className={css.ratingContainer}>
+              <p className={css.textextRating}>⭐ {item.rating}</p>
+              <p className={css.textPrice}>${item.price_per_hour}/hour</p>
+            </div>
+            <div className={css.heart}>
+              <svg
+                width="25"
+                height="22"
+                viewBox="0 0 25 22"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M21.8989 2.74614C21.3456 2.19257 20.6886 1.75343 19.9655 1.45382C19.2425 1.15421 18.4674 1 17.6847 1C16.902 1 16.127 1.15421 15.4039 1.45382C14.6809 1.75343 14.0239 2.19257 13.4706 2.74614L12.3222 3.89448L11.1739 2.74614C10.0562 1.62848 8.54035 1.00058 6.95973 1.00058C5.37911 1.00058 3.86323 1.62848 2.74557 2.74614C1.6279 3.86381 1 5.37969 1 6.96031C1 8.54093 1.6279 10.0568 2.74557 11.1745L3.8939 12.3228L12.3222 20.7511L20.7506 12.3228L21.8989 11.1745C22.4525 10.6212 22.8916 9.96419 23.1912 9.24111C23.4908 8.51803 23.645 7.74301 23.645 6.96031C23.645 6.17762 23.4908 5.40259 23.1912 4.67951C22.8916 3.95643 22.4525 3.29946 21.8989 2.74614Z"
+                  stroke="#191A15"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
           </div>
         </div>
         <h3 className={css.name}>{item.name}</h3>
@@ -48,8 +52,35 @@ export default function PsychologistCard({ item }: { item: Psychologist }) {
           <li className={css.listItem}>{item.initial_consultation}</li>
         </ul>
         <p className={css.textAbout}>{item.about}</p>
+        <button
+          className={css.readMoreBtn}
+          onClick={() => setExpanded(!expanded)}
+        >
+          {expanded ? "Read less" : "Read more"}
+        </button>
+        {expanded && (
+          <div className={css.reviews}>
+            {item.reviews.map((review, index) => (
+              <div key={index} className={css.reviewItem}>
+                <div className={css.reviewHeader}>
+                  <div className={css.avatarSmall}>
+                    {review.reviewer.charAt(0)}
+                  </div>
+
+                  <div>
+                    <p className={css.reviewName}>{review.reviewer}</p>
+                    <p className={css.reviewRating}>⭐ {review.rating}</p>
+                  </div>
+                </div>
+
+                <p className={css.reviewText}>{review.comment}</p>
+              </div>
+            ))}
+
+            <button className={css.bookBtn}>Make an appointment</button>
+          </div>
+        )}
       </div>
     </div>
-    // </div>
   );
 }
--- types/psychologists.ts
+++ types/psychologists.ts
@@ -1,13 +1,19 @@
 export type Psychologist = {
-    id: string;
-    name: string;
-    avatar_url: string;
-    experience: number;
-    reviews: number;
-    price_per_hour: number;
-    rating: number;
-    license: string;
-    specialization: string;
-    initial_consultation: boolean;
-    about: string;
-  };
+  id: string;
+  name: string;
+  avatar_url: string;
+  experience: number;
+  reviews: Reviews[];
+  price_per_hour: number;
+  rating: number;
+  license: string;
+  specialization: string;
+  initial_consultation: boolean;
+  about: string;
+};
+
+export type Reviews = {
+  reviewer: string;
+  rating: number;
+  comment: string;
+};
