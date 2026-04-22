b5d33770f2cd7f9bf4036eefb771ce542ec7dd90
Anna Sytenko <anna_sytenko91@icloud.com>
2026-04-22 19:26:10 +0700

    fixed bug in psychologists page were not displaying correctly due to a async opperations, loading state.

--- app/psychologists/page.tsx
+++ app/psychologists/page.tsx
@@ -5,28 +5,47 @@ import PsychologistsList from "@/components/Psychologists/PsychologistsList";
 import { getPsychologists } from "@/lib/api/api";
 import { Psychologist } from "@/types/psychologists";
 import { useEffect, useState } from "react";
-import css from "./PsychologistsPage.module.css"
+import css from "./PsychologistsPage.module.css";
 
 export default function Page() {
   const [data, setData] = useState<Psychologist[]>([]);
   const [limit, setLimit] = useState(3);
   const [sort, setSort] = useState("name-asc");
-  
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    getPsychologists(limit).then(setData);
+    const fetchData = async () => {
+      setLoading(true);
+
+      const result = await getPsychologists(limit);
+      setData(result);
+
+      setLoading(false);
+    };
+
+    fetchData();
   }, [limit]);
+
+  if (loading) {
+    return (
+      <div className={css.loaderWrapper}>
+        <div className={css.loader}></div>
+      </div>
+    );
+  }
 
   return (
     <div className="container">
       <Fillter onChange={setSort} />
-
       <PsychologistsList data={data} sort={sort} />
-     <div className={css.btnContainer}>
-      <button className={css.loadMoreBtn} onClick={() => setLimit((prev) => prev + 3)}>
-        Load more
-      </button>
-     </div>
+      <div className={css.btnContainer}>
+        <button
+          className={css.loadMoreBtn}
+          onClick={() => setLimit((prev) => prev + 3)}
+        >
+          Load more
+        </button>
+      </div>
     </div>
   );
 }
